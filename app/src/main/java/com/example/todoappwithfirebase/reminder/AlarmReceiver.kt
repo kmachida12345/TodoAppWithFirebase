3bb87102db7e759aa57afd7bd6eefd327c6162f7
machida.kohei <kmachida12345@gmail.com>
2021-01-22 14:49:02 +0900

    Alarm発火したらNotification表示

--- app/src/main/java/com/example/todoappwithfirebase/reminder/AlarmReceiver.kt
+++ app/src/main/java/com/example/todoappwithfirebase/reminder/AlarmReceiver.kt
@@ -1,13 +1,53 @@
 package com.example.todoappwithfirebase.reminder
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
+import com.example.todoappwithfirebase.R
 
 class AlarmReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         Log.d("hoge", "onReceive: ")
 
+        val notificationId = 114514
+
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        val name = "channel name desu"
+        val descriptionText = "channel no description desu"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        // Register the channel with the system
+        val notificationManager: NotificationManager =
+            getSystemService(context!!, NotificationManager::class.java) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+
+
+        var builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_task_alt_24)
+            .setContentTitle("test title")
+            .setContentText("test description")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+
+        with(NotificationManagerCompat.from(context)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(notificationId, builder.build())
+        }
+
     }
+
+    companion object {
+        private const val CHANNEL_ID = "todo app desuyo"
+        private const val TAG = "AlarmReceiver"
+    }
+
 }
