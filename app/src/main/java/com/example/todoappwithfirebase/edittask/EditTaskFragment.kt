875c1b3be923c629f4568b188f002a6870ea09c7
machi <kmachida12345@gmail.com>
2021-03-09 22:56:42 +0900

    WIP:MVVMでリファクタリング

--- app/src/main/java/com/example/todoappwithfirebase/edittask/EditTaskFragment.kt
+++ app/src/main/java/com/example/todoappwithfirebase/edittask/EditTaskFragment.kt
@@ -1,35 +1,21 @@
 package com.example.todoappwithfirebase.edittask
 
-import android.app.AlarmManager
-import android.app.PendingIntent
-import android.app.PendingIntent.FLAG_ONE_SHOT
-import android.content.Context.ALARM_SERVICE
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import android.widget.TimePicker
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import com.example.todoappwithfirebase.MyViewModel
-import com.example.todoappwithfirebase.R
 import com.example.todoappwithfirebase.databinding.FragmentEditTaskBinding
-import com.example.todoappwithfirebase.model.Task
-import com.example.todoappwithfirebase.reminder.AlarmReceiver
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.util.*
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 
 @AndroidEntryPoint
-class EditTaskFragment: Fragment() {
+class EditTaskFragment : Fragment() {
 
     private val viewModel: EditTaskViewModel by viewModels()
 
@@ -37,47 +23,15 @@ class EditTaskFragment: Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val binding = FragmentEditTaskBinding.inflate(inflater, container, false)
 
         binding.viewModel = viewModel
 
-        // FIXME: DataBindingにかきかえ
-//        binding.root.findViewById<Button>(R.id.confirm).setOnClickListener {
-//            lifecycleScope.launch(Dispatchers.IO) {
-//
-//                val calNow: Calendar = Calendar.getInstance()
-//                val calSet: Calendar = calNow.clone() as Calendar
-//
-//                calSet.set(Calendar.HOUR_OF_DAY, binding.root.findViewById<TimePicker>(R.id.remind_time_picker).hour)
-//                calSet.set(Calendar.MINUTE, binding.root.findViewById<TimePicker>(R.id.remind_time_picker).minute)
-//                calSet.set(Calendar.SECOND, 0)
-//                calSet.set(Calendar.MILLISECOND, 0)
-//
-////                if (calSet.compareTo(calNow) <= 0) {
-////                    // Today Set time passed, count to tomorrow
-////                    calSet.add(Calendar.DATE, 1)
-////                }
-//
-//                val task = Task(0, view.findViewById<TextView>(R.id.title).text.toString(), dueTime = calSet.timeInMillis)
-//
-////                val id = viewModel.insert(task)
-//
-//                val alarmManager =
-//                    requireContext().getSystemService(ALARM_SERVICE) as? AlarmManager
-//
-//                val alarmIntent = Intent(context, AlarmReceiver::class.java).let { intent ->
-//                    intent.putExtra("taskId", id)
-//                    PendingIntent.getBroadcast(context, 0, intent, FLAG_ONE_SHOT)
-//                }
-//                alarmManager?.set(AlarmManager.RTC, task.dueTime, alarmIntent)
-//
-//                launch(Dispatchers.Main) {
-//                    Toast.makeText(context, "added", Toast.LENGTH_LONG).show()
-//                }
-//                findNavController().popBackStack()
-//            }
-//        }
+        viewModel.message.onEach {
+            findNavController().popBackStack()
+        }.launchIn(lifecycleScope)
+
         return binding.root
     }
 
--- app/src/main/java/com/example/todoappwithfirebase/edittask/EditTaskViewModel.kt
+++ app/src/main/java/com/example/todoappwithfirebase/edittask/EditTaskViewModel.kt
@@ -1,19 +1,70 @@
 package com.example.todoappwithfirebase.edittask
 
+import android.app.AlarmManager
 import android.app.Application
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_ONE_SHOT
+import android.content.Intent
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todoappwithfirebase.model.Task
 import com.example.todoappwithfirebase.model.TaskRepository
+import com.example.todoappwithfirebase.reminder.AlarmReceiver
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
-class EditTaskViewModel @Inject constructor(application: Application, private val mRepository: TaskRepository): AndroidViewModel(application) {
+class EditTaskViewModel @Inject constructor(
+    private val _application: Application,
+    private val mRepository: TaskRepository
+) : AndroidViewModel(_application) {
     val title = MutableLiveData("")
     val desc = MutableLiveData("")
+    private val alarmManager: AlarmManager =
+        getSystemService(_application, AlarmManager::class.java)!!
+
+    /** メッセージを送出する [Flow]。 */
+    val message: SharedFlow<Message>
+        get() = _message
+    private val _message = MutableSharedFlow<Message>()
 
     fun insert() {
+        val calNow: Calendar = Calendar.getInstance()
+        val calSet: Calendar = calNow.clone() as Calendar
 
+        val task = Task(
+            0,
+            title.value ?: "",
+            description = desc.value ?: "",
+            dueTime = calSet.timeInMillis
+        )
+
+        Timber.d("hoge ${title.value}, ${desc.value}")
+
+        viewModelScope.launch(Dispatchers.Main) {
+            val id = mRepository.insert(task)
+
+            val alarmIntent = Intent(_application, AlarmReceiver::class.java).let { intent ->
+                intent.putExtra("taskId", id)
+                PendingIntent.getBroadcast(_application, 0, intent, FLAG_ONE_SHOT)
+            }
+            alarmManager.set(AlarmManager.RTC, task.dueTime, alarmIntent)
+
+            _message.emit(Message.Done)
+
+        }
     }
+
+    sealed class Message {
+        object Done : Message()
+    }
+
 }
